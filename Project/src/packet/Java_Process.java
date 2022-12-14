726a00efcf8a5f1d3df855037fa9a83d674ddfbc
Mazhar Akyoldaş <38635777+akyoldash@users.noreply.github.com>
2022-12-15 00:02:46 +0300

    Add files via upload

--- Project/src/packet/Java_Process.java
+++ Project/src/packet/Java_Process.java
@@ -1,9 +1,13 @@
 package packet;
+import static packet.ColoredSystemOutPrintln.*;
 
 public class Java_Process {
+
+
 	//Her proses için bu program çalıştırılır
 	public static void main(String[] args) {
+		int colorId = Integer.parseInt(args[2]);
 		long pid = ProcessHandle.current().pid();
-		System.out.print("(id:" + pid + "  oncelik:" + args[0] + "  kalan sure:" + args[1] + " sn)");
+		System.out.println(COLORS[colorId] + "(id:" + pid + "  oncelik:" + args[0] + "  kalan sure:" + args[1] + " sn)" + ANSI_RESET);
 	}
 }
